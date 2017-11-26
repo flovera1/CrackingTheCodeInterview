a6539c7307813a84d60ce16efb3d34dc2213e6e2
flovera1 <flovera1@gmail.com>
2017-11-26 08:27:15 -0400

    another way of think about interviews

--- introduction/math_puzzles/isPrime.py
+++ introduction/math_puzzles/isPrime.py
@@ -1,10 +1,16 @@
 def isPrime(number):
     if number < 2:
         return False
-    for i in number:
-        if (i%2 == 0):
+    for i in range(2,number):
+        if (number%i == 0):
             return False
     return True
 
 
-        
+def main():
+    number = 4
+    n      = isPrime(number)      
+    print(n)
+
+if __name__ == "__main__":
+    main()
